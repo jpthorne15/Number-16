dc5ae5595d16e403526117e547079951c5fde3f4
Jim Thorne <jamesthorne@Jamess-MacBook-Air.local>
2020-01-08 19:08:09 -0700

    first commit

---
+++ number16.js
@@ -0,0 +1,13 @@
+// Boolean expression
+let age;
+let shoeSize;
+
+// Data is input from user in a Webform
+
+age = 12;
+shoeSize = 12;
+
+if (age === shoeSize) {
+    console.log ("Act your age and your shoe size")
+}
+// Expected output "Act your age and your show size"
